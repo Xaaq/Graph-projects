8f59783c192bece13ed4bc9e33542d460645d54d
phpl <pawehadam9@gmail.com>
2017-03-07 13:46:23 +0100

    test comment intellij

--- Graph Project One/src/graphPackage/Graph.java
+++ Graph Project One/src/graphPackage/Graph.java
@@ -1,97 +1,94 @@
 package graphPackage;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
-import java.awt.Point;
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Graph {
 
-	//przechowuje graf w postaci macierzy
-	private int[][] graphMatrix = null;
-	//odnosnik do panelu w ktorym bedzie wypisywac liczbowa reprezentacje grafu
-	private JPanel writePanel;
+    //przechowuje graf w postaci macierzy
+    private int[][] graphMatrix = null;
+    //odnosnik do panelu w ktorym bedzie wypisywac liczbowa reprezentacje grafu
+    private JPanel writePanel;
 
-	//konstruktor przyjmujacy jako argument JFrame do ktorego bedzie wpisywana macierzowa reprezentacja grafu
-	public Graph(JPanel writePanel) {
-		this.writePanel = writePanel;
-	}
+    //konstruktor przyjmujacy jako argument JFrame do ktorego bedzie wpisywana macierzowa reprezentacja grafu
+    public Graph(JPanel writePanel) {
+        this.writePanel = writePanel;
+    }
 
-	//generuje graf w postaci macierzy z zadanym prawdopodobienstwem wstapienia krawedzi
-	public void generateProbabilityMatrix(int size, double probability) {
-		graphMatrix = new int[size][size];
+    //generuje graf w postaci macierzy z zadanym prawdopodobienstwem wstapienia krawedzi
+    public void generateProbabilityMatrix(int size, double probability) {
+        graphMatrix = new int[size][size];
 
-		for (int i = 0; i < graphMatrix.length; i++) {
-			for (int j = i; j < graphMatrix[i].length; j++) {
-				if (i == j) {
-					graphMatrix[i][j] = 0;
-					continue;
-				}
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = i; j < graphMatrix[i].length; j++) {
+                if (i == j) {
+                    graphMatrix[i][j] = 0;
+                    continue;
+                }
 
-				if (Math.random() <= probability)
-					graphMatrix[i][j] = 1;
-				else
-					graphMatrix[i][j] = 0;
-			}
-		}
+                if (Math.random() <= probability)
+                    graphMatrix[i][j] = 1;
+                else
+                    graphMatrix[i][j] = 0;
+            }
+        }
 
-		for (int i = 0; i < graphMatrix.length; i++) {
-			for (int j = 0; j < i; j++) {
-				graphMatrix[i][j] = graphMatrix[j][i];
-			}
-		}
-	}
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                graphMatrix[i][j] = graphMatrix[j][i];
+            }
+        }
+    }
 
-	//generuje graf w postaci macierzy z podana liczba krawedzi
-	public void generateNumberMatrix(int size, double numberOfLines) {
-		int number = (int) numberOfLines;
-		graphMatrix = new int[size][size];
+    //generuje graf w postaci macierzy z podana liczba krawedzi
+    public void generateNumberMatrix(int size, double numberOfLines) {
+        int number = (int) numberOfLines;
+        graphMatrix = new int[size][size];
 
-		ArrayList<Point> pointList = new ArrayList<Point>();
-		int lineCount = 0;
+        ArrayList<Point> pointList = new ArrayList<Point>();
+        int lineCount = 0;
 
-		for (int i = 0; i < graphMatrix.length; i++) {
-			for (int j = i + 1; j < graphMatrix[i].length; j++) {
-				pointList.add(new Point(i, j));
-			}
-		}
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = i + 1; j < graphMatrix[i].length; j++) {
+                pointList.add(new Point(i, j));
+            }
+        }
 
-		while (!pointList.isEmpty() && lineCount < number) {
-			int randomIndex = (int) (Math.random() * (pointList.size()));
-			int x = (int) pointList.get(randomIndex).getX();
-			int y = (int) pointList.get(randomIndex).getY();
-			pointList.remove(randomIndex);
+        while (!pointList.isEmpty() && lineCount < number) {
+            int randomIndex = (int) (Math.random() * (pointList.size()));
+            int x = (int) pointList.get(randomIndex).getX();
+            int y = (int) pointList.get(randomIndex).getY();
+            pointList.remove(randomIndex);
 
-			graphMatrix[x][y] = 1;
-			lineCount++;
-		}
-		
-		for (int i = 0; i < graphMatrix.length; i++) {
-			for (int j = 0; j < i; j++) {
-				graphMatrix[i][j] = graphMatrix[j][i];
-			}
-		}
-	}
+            graphMatrix[x][y] = 1;
+            lineCount++;
+        }
 
-	//funkcja robiaca grid w JFramie (podanym w konstruktorze) i wpisujaca do niego macierz
-	public void writeMatrix() {
-		if (graphMatrix == null)
-			return;
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                graphMatrix[i][j] = graphMatrix[j][i];
+            }
+        }
+    }
 
-		writePanel.removeAll();
-		writePanel.setLayout(new GridLayout(graphMatrix.length, graphMatrix[0].length, 10, 10));
+    //funkcja robiaca grid w JFramie (podanym w konstruktorze) i wpisujaca do niego macierz
+    public void writeMatrix() {
+        if (graphMatrix == null)
+            return;
 
-		for (int i = 0; i < graphMatrix.length; i++) {
-			for (int j = 0; j < graphMatrix[i].length; j++) {
-				JLabel label = new JLabel(Integer.toString(graphMatrix[i][j]));
-				GridBagConstraints gridInfo = new GridBagConstraints();
-				gridInfo.gridx = i;
-				gridInfo.gridy = j;
-				writePanel.add(label, gridInfo);
-			}
-		}
-	}
+        writePanel.removeAll();
+        writePanel.setLayout(new GridLayout(graphMatrix.length, graphMatrix[0].length, 10, 10));
+
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = 0; j < graphMatrix[i].length; j++) {
+                JLabel label = new JLabel(Integer.toString(graphMatrix[i][j]));
+                GridBagConstraints gridInfo = new GridBagConstraints();
+                gridInfo.gridx = i;
+                gridInfo.gridy = j;
+                writePanel.add(label, gridInfo);
+            }
+        }
+        //test
+    }
 }
