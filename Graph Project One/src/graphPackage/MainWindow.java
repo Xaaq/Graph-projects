d108fbacee1b3279dae8e84e482b5a24fed51b33
Xaaq <Xaaq333@gmail.com>
2017-03-09 02:39:34 +0100

    poprawki w kodzie ogolnym, zrobienie ZE JEST TERAZ CHOCIAZ TROCHE CZYTELNY

--- Graph Project One/src/graphPackage/MainWindow.java
+++ Graph Project One/src/graphPackage/MainWindow.java
@@ -3,6 +3,7 @@ package graphPackage;
 import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+
 /**
  * Created by pawel on 07.03.17.
  */
@@ -22,18 +23,20 @@ class MainWindow {
     private final Graph graph = new Graph();
 
     private MainWindow() {
-        generujButton.addActionListener( actionEvent -> {
+        generujButton.addActionListener(actionEvent -> {
             try {
                 //graf
                 int size = Integer.parseInt(wielkoscTextField.getText());
                 double probability = Double.parseDouble(prawdopodobienstwoTextField.getText());
                 String choose = String.valueOf(comboBox2.getSelectedItem());
+
                 //generacja
-                if (choose.equals( "Ilosc krawedzi" )) {
-                    graph.generateNumberMatrix( size, probability );
+                if (choose.equals("Ilosc krawedzi")) {
+                    graph.generateNumberMatrix(size, probability);
                 } else {
-                    graph.generateProbabilityMatrix( size, probability );
+                    graph.generateProbabilityMatrix(size, probability);
                 }
+
                 subPan1.remove(circleGraph1);
                 circleGraph1 = new CircleGraph(graph);
                 subPan1.add(circleGraph1);
@@ -43,25 +46,26 @@ class MainWindow {
             } catch (NumberFormatException exception) {
                 System.out.println(exception.getMessage());
             }
-        } );
-        zapiszDoPlikuButton.addActionListener( actionEvent -> {
-//                pola tekstowe -- bedzie do zapisywania pliku
+        });
+        zapiszDoPlikuButton.addActionListener(actionEvent -> {
+            //pola tekstowe -- bedzie do zapisywania pliku
             StringBuilder s = new StringBuilder();
             for (int[] x : graph.getGraphMatrix()) {
-                s.append( "|" );
+                s.append("|");
                 for (int y : x) {
-                    s.append( " " );
-                    s.append( y );
-                    s.append( " " );
+                    s.append(" ");
+                    s.append(y);
+                    s.append(" ");
                 }
-                s.append( "|\n" );
+                s.append("|\n");
             }
-            try(  PrintWriter out = new PrintWriter( "graf.txt" )  ){
+
+            try (PrintWriter out = new PrintWriter("graf.txt")) {
                 out.println(s.toString());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-        } );
+        });
     }
 
     public static void main(String[] args) {
@@ -73,6 +77,6 @@ class MainWindow {
     }
 
     private void createUIComponents() {
-        circleGraph1 = new CircleGraph(0); // potrzebny do wygenerowania programu - graf o 0 wierzcholkach
+        circleGraph1 = new CircleGraph(0); //potrzebny do wygenerowania programu - graf o 0 wierzcholkach
     }
 }
