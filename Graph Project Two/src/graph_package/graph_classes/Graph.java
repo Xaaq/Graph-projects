701103b4dcfc2b9cc5ed1da02e2724fa62bb6571
Xaaq <Xaaq333@gmail.com>
2017-03-12 22:42:13 +0100

    PO WIELKICH TRUDACH zrobiona funkcja generujaca macierz z sekwencji liczb

--- Graph Project Two/src/graph_package/graph_classes/Graph.java
+++ Graph Project Two/src/graph_package/graph_classes/Graph.java
@@ -1,7 +1,7 @@
 package graph_package.graph_classes;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by Xaaq333 on 2017-03-11.
@@ -9,52 +9,109 @@ import java.util.Collections;
 //przechowuje graf w postaci macierzy i funkcje z nim zwiazane
 public class Graph {
 
+    //przechowuje graf w formie macierzy
     private int[][] graphMatrix;
 
+    //konstruktor
     public Graph() {
 
     }
 
-    //sprawdza czy mozna zrobic graf z podanej sekwencji liczb (przyjmuje tablice intow)
-    public boolean checkNumberSeqence(int[] inputNumberSequence) {
-        ArrayList<Integer> numberSequece = new ArrayList<>();
-
-        for (int inputNumber : inputNumberSequence) {
-            numberSequece.add(inputNumber);
-        }
-
-        return checkNumberSeqence(numberSequece);
+    //zwraca graf w posatci macierzy
+    public int[][] getGraphMatrix() {
+        return graphMatrix.clone();
     }
 
-    //sprawdza czy mozna zrobic graf z podanej sekwencji liczb (przyjmuje arraylist intow)
-    public boolean checkNumberSeqence(ArrayList<Integer> inputNumberSequence) {
-        //TODO: zrobic zeby funkcja robila od razu macierz grafowa
-        if (inputNumberSequence.isEmpty())
-            return true;
+    //sub-klasa ktora opakowywuje wierzcholek i liczbe polaczonych z nim krawedzi (potrzebna do tworzenia grafu)
+    private class Verticle {
+        public final int verticleNumber;
+        public int connectionCount;
 
-        ArrayList<Integer> numberSequence = (ArrayList<Integer>) inputNumberSequence.clone();
-        Collections.sort(numberSequence);
-        int lastNumber = numberSequence.get(numberSequence.size() - 1);
-        numberSequence.remove(numberSequence.size() - 1);
+        Verticle(int verticleNumber, int connectionCount) {
+            this.verticleNumber = verticleNumber;
+            this.connectionCount = connectionCount;
+        }
+    }
 
-        for (int i = numberSequence.size() - 1; i >= 0; i--) {
-            if (numberSequence.get(i) <= 0)
-                numberSequence.remove(i);
+    //sub-klasa ktora sluzy do porownania obiektow klasy Verticle
+    private class VerticleComparator implements Comparator<Verticle> {
+        @Override
+        public int compare(Verticle verticle1, Verticle verticle2) {
+            if (verticle1.connectionCount > verticle2.connectionCount)
+                return 1;
+            if (verticle1.connectionCount < verticle2.connectionCount)
+                return -1;
+            return 0;
+        }
+    }
+
+    //robi graf z podanej sekwencji liczb jesli sie da
+    public void checkNumberSequence(int[] inputNumberSequence) {
+        ArrayList<Verticle> verticleSequece = new ArrayList<>();
+
+        for (int i = 0; i < inputNumberSequence.length; i++) {
+            verticleSequece.add(new Verticle(i, inputNumberSequence[i]));
         }
 
-        if (lastNumber > numberSequence.size())
-            return false;
+        int[][] tempMatrix = checkNumberSequence(verticleSequece);
 
-        for (int i = 1; i <= lastNumber; i++) {
-            int actualNumber = numberSequence.get(numberSequence.size() - i) - 1;
-            numberSequence.set(numberSequence.size() - i, actualNumber);
+        if (tempMatrix != null)
+            graphMatrix = tempMatrix;
+    }
+
+    //sprawdza czy mozna zrobic graf z podanej sekwencji liczb
+    private int[][] checkNumberSequence(ArrayList<Verticle> inputVerticleSequence) {
+        ArrayList<Verticle> verticleSequence = (ArrayList<Verticle>) inputVerticleSequence.clone();
+        int highestVerticleNumber = 0;
+
+        //sprawdza jaki jest najwieksze id wierzcholka (zeby wiedziec jak duza tablice zrobic)
+        for (Verticle verticle : verticleSequence) {
+            if (verticle.verticleNumber > highestVerticleNumber)
+                highestVerticleNumber = verticle.verticleNumber;
         }
 
-        for (int i = numberSequence.size() - 1; i >= 0; i--) {
-            if (numberSequence.get(i) <= 0)
-                numberSequence.remove(i);
+        highestVerticleNumber++;
+        int[][] outputArray = new int[highestVerticleNumber][highestVerticleNumber];
+
+        //wypelnia tablice zerami
+        for (int i = 0; i < highestVerticleNumber; i++) {
+            for (int j = 0; j < highestVerticleNumber; j++) {
+                outputArray[i][j] = 0;
+            }
+        }
+        //usuwa wsyzstkie zerowe pozycje z listy
+        for (int i = verticleSequence.size() - 1; i >= 0; i--) {
+            if (verticleSequence.get(i).connectionCount <= 0)
+                verticleSequence.remove(i);
         }
 
-        return checkNumberSeqence(numberSequence);
+        //jesli lista jest pusta zwraca pusta tablice
+        if (verticleSequence.isEmpty())
+            return outputArray;
+
+        verticleSequence.sort(new VerticleComparator());
+        Verticle lastVerticle = verticleSequence.get(verticleSequence.size() - 1);
+        verticleSequence.remove(verticleSequence.size() - 1);
+
+        //jesli trzeba wygenerowac wiecej krawedzi niz jest pozycji w liscie - nie da sie wygenerowac tablicy
+        if (lastVerticle.connectionCount > verticleSequence.size())
+            return null;
+
+        //odejmuje od ostatnich pozycji z listy wartosc 1 i dodaje znak w tablicy
+        for (int i = 1; i <= lastVerticle.connectionCount; i++) {
+            int actualNumber = verticleSequence.get(verticleSequence.size() - i).connectionCount - 1;
+            verticleSequence.get(verticleSequence.size() - i).connectionCount = actualNumber;
+
+            outputArray[lastVerticle.verticleNumber][verticleSequence.get(verticleSequence.size() - i).verticleNumber] = 1;
+            outputArray[verticleSequence.get(verticleSequence.size() - i).verticleNumber][lastVerticle.verticleNumber] = 1;
+        }
+
+        int[][] recursiveArray = checkNumberSequence(verticleSequence);
+
+        //jesli otrzymano tablice z funkcji rekurencyjnej - dodaj do biezacej tablicy i zwroc wynik
+        if (recursiveArray != null)
+            return HelperClass.addSquareMatrixes(outputArray, recursiveArray);
+        else
+            return null;
     }
 }
