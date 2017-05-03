44ead9f55ef45ab27ebc735b590fe66273cf46f5
mgacek8 <mgacek8@wp.pl>
2017-05-03 23:11:08 +0200

    proba poprawienia błędu

--- Graph Project Two/src/graph_package/graph_classes/DFSPaths.java
+++ Graph Project Two/src/graph_package/graph_classes/DFSPaths.java
@@ -1,5 +1,6 @@
 package graph_package.graph_classes;
 
+
 /**
  * Created by Mateusz on 18.03.2017.
  */
@@ -18,26 +19,38 @@ public class DFSPaths {
         this.startedVertex = startedVertex;
         edgeTo = new int[graph.getNodeGraphLength()];
         marked = new boolean[graph.getNodeGraphLength()];
+        graph.generateNodeArray();
         dfs_recursive(graph, startedVertex);
     }
 
     private void dfs_recursive(Graph graph, int vertex) {
         // oznaczamy wierzchołek, jako oznaczony
+        System.out.println("vertex: " + vertex);
         marked[vertex] = true;
         //Pobieramy wierzchołek
         GraphNode node = graph.getGraphNode(vertex);
+
         // odwiedzamy każdy sąsiedni nieodwiedzony wierzchołek i zapisujemy trase
         for (GraphNode eachVertex : node.getConnectionList()) {
             // sprawdzamy czy sąsiadem naszego wierzchołka jest punkt startowy,
             // oraz czy wszystkie wierzchołki są już odwiedzone
-            if (eachVertex.getId() == startedVertex && isAllMarked())
+            if (eachVertex.getId() == startedVertex && isAllMarked()){
                 isHamilton = true;
+                System.out.println(":: " + eachVertex.getId());
+                return;
+            }
+
+            System.out.println("vertex: "  + vertex + ": m["+eachVertex.getId() + "] = " +marked[eachVertex.getId()]);
 
             //jeżeli nieodwiedzony
             if (!marked[eachVertex.getId()]) {
                 edgeTo[eachVertex.getId()] = vertex;
                 dfs_recursive(graph, eachVertex.getId());
             }
+            System.out.println("::: "+eachVertex.getId());
+
+//            marked[eachVertex.getId()] = false;
+
         }
     }
 
